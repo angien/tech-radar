5c6551325489c4e66c222ef54e7dde0026956a13
ryliao0221 <ryliao0221@gmail.com>
2014-10-06 05:58:56 -0700

    display json

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(serveIndex(__dirname + '/public'));
 
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 8080);
 
 console.log('Server running on port 80');
