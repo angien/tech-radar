19e771ecf9bbabd0c3086e117e5eb525b8218606
Angie Nguyen <anginguy@cisco.com>
2014-10-06 09:03:42 -0700

    asdfa

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(serveIndex(__dirname + '/public'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 80);
 
 console.log('Server running on port 80');
